1d668de28f28ab8c86d0e09b75fd692acbda5b40
DivyaDeepthi Thota <divyadeepthithota@DivyaDeepthis-MacBook-Pro.local>
2022-09-20 06:35:57 +0530

    Test MEssage

---
+++ src/main/java/com/myapp/controller/JewelleryShopController.java
@@ -0,0 +1,31 @@
+package com.myapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myapp.model.Product;
+import com.myapp.service.JewelleryShopService;
+
+@RestController
+public class JewelleryShopController {
+
+	private final JewelleryShopService jewelleryShopService;
+
+	@Autowired
+	public JewelleryShopController(JewelleryShopService jewelleryShopService) {
+		this.jewelleryShopService = jewelleryShopService;
+	}
+
+	@RequestMapping(value = "/getDiscount", method = RequestMethod.GET, produces = "application/json")
+	public Product getQuestions(@RequestParam(required = true) String type) {
+
+		Product product = new Product();
+		System.out.println("GOLDDDD"+type);
+		product.setType(type);
+		jewelleryShopService.getProductDiscount(product);
+		return product;
+	}
+}
